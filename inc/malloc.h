87942f369349a5af3618d7a4a02943c7412195b3
tvisenti <tvisenti@student.42.fr>
2017-09-15 16:43:03 +0200

    [Malloc]: Init files

--- inc/malloc.h
+++ inc/malloc.h
@@ -6,7 +6,7 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/14 10:51:23 by tvisenti          #+#    #+#             */
-/*   Updated: 2017/09/14 16:19:53 by tvisenti         ###   ########.fr       */
+/*   Updated: 2017/09/15 16:28:19 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
 #define MAX_SIZE_ALLOC 10000000000
 
-#define TINY_SIZE 992
-#define TINY_AREA 16
+#define TINY_SIZE getpagesize() / 2 //n
+#define TINY_AREA 128 // N
 
-#define SMALL_SIZE 126999
-#define SMALL_AREA 512
+#define SMALL_SIZE getpagesize() * 10 // m
+#define SMALL_AREA 4096 // M
 
-#define LARGE_SIZE 127000
+typedef struct		s_block
+{
+	// void			*mem;
+	size_t			size;
+	int				isFree;
+	struct s_block	*next;
+}					t_block;
 
+typedef struct		s_page
+{
+	struct s_block	*tiny;
+	// struct s_block	*tinyEnd;
+	struct s_block	*small;
+	// struct s_block	*smallEnd;
+	struct s_block	*large;
+	// struct s_block	*largeEnd;
+}					t_page;
 
 void	free(void *ptr);
-void	*malloc(size_t size);
+void	*my_malloc(size_t size);
 void	*realloc(void *ptr, size_t size);
 void	show_alloc_mem();
--- src/malloc.c
+++ src/malloc.c
@@ -6,20 +6,28 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/14 10:50:56 by tvisenti          #+#    #+#             */
-/*   Updated: 2017/09/14 13:56:47 by tvisenti         ###   ########.fr       */
+/*   Updated: 2017/09/15 14:26:39 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/malloc.h"
 
-void	*malloc(size_t size)
+void	*my_malloc(size_t size)
 {
-	if (size == 0 || size == NULL || size > MAX_SIZE_ALLOC)
+	void	*p;
+	if (size == 0 || size > MAX_SIZE_ALLOC)
 		return NULL;
-	
+	p = mmap(0, size + 1, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	return p;
 }
 
 int	main(void)
 {
+	char *str;
+
+	printf("\n\nPageSize: %d\n", getpagesize());
+	str = (char*)my_malloc(16);
+	// str = "je teste caldgnnelg";
+	printf("str: %p", str);
 	return (0);
 }
